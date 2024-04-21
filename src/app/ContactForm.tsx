4c22de1948008acd08bfeb49dba2828f99abb0eb
verdes ben <verdesb.devacc@gmail.com>
2024-04-21 17:58:49 +0200

    test port3001

--- src/app/ContactForm.tsx
+++ src/app/ContactForm.tsx
@@ -51,7 +51,7 @@ export default function ContactForm() {
     async function handleSubmit(event:React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         if (validate()) {
-            const response = await fetch('https://www.bennydev.fr/api/send', {
+            const response = await fetch('/api/send', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
