63fc336d7dd9a56486c4c0cf09e439fdc6c40381
verdes ben <verdesb.devacc@gmail.com>
2024-04-21 15:22:14 +0200

    fix referer

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -28,7 +28,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
      const referer = request.headers.get('referer');
 
      // if (referer !== 'http://localhost:3000/') {
-         if (referer !== 'http://www.bennydev.fr/') {
+         if (referer !== 'https://www.bennydev.fr') {
          return Response.json('Unauthorized')
      } else {
          if (await rateLimit(request)) {
