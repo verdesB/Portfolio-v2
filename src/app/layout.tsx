29fb840ee317e7d61a6b23912afc5897b839cec8
verdes ben <verdesb.devacc@gmail.com>
2024-04-23 10:46:32 +0200

    import react(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
+import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {Toaster} from "@/app/components/ui/Toaster";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfolio - Verdès Benjamin",
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body >{children}</body>
+      <body className={inter.className}>{children}</body>
 
     </html>
   );
