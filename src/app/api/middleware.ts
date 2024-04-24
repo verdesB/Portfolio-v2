91f112024c3a4da1e8bd6386a20205e3f390fd67
verdes ben <verdesb.devacc@gmail.com>
2024-04-24 18:48:48 +0200

    delete middlewareé

--- src/app/api/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { match } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
-
-let headers = { 'accept-language': 'en-US,en;q=0.5' }
-let languages = new Negotiator({ headers }).languages()
-let locales = ['fr-FR', 'en-US', 'en']
-let defaultLocale = 'fr-FR'
-
-match(languages, locales, defaultLocale) // -> 'en-US'git add  .
